c1b983d68667b101a449c43a28fad6baeef95105
Oleksandr <102870331+Baryzhyk@users.noreply.github.com>
2025-08-14 13:32:01 +0300

    Create blockassist.sh

---
+++ blockassist.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Перевірка наявності необхідних утиліт, встановлення за потреби
+if ! command -v figlet &> /dev/null; then
+    echo "figlet не знайдено. Встановлюємо..."
+    sudo apt update && sudo apt install -y figlet
+fi
+
+if ! command -v whiptail &> /dev/null; then
+    echo "whiptail не знайдено. Встановлюємо..."
+    sudo apt update && sudo apt install -y whiptail
+fi
+
+# Кольори
+GOLD='\033[38;5;220m'
+NC='\033[0m' # Скидання кольору
+
+# Завантаження та відображення логотипу
+bash <(curl -s https://raw.githubusercontent.com/Baryzhyk/nodes/refs/heads/main/logo.sh)
+
+# Анімація завантаження
+animate_loading() {
+    for ((i = 1; i <= 5; i++)); do
+        printf "\r${GOLD}Завантажуємо меню${NC}."
+        sleep 0.3
+        printf "\r${GOLD}Завантажуємо меню${NC}.."
+        sleep 0.3
+        printf "\r${GOLD}Завантажуємо меню${NC}..."
+        sleep 0.3
+        printf "\r${GOLD}Завантажуємо меню${NC}"
+        sleep 0.3
+    done
+    echo ""
+}
+
+# Меню вибору
+CHOICE=$(whiptail --title "Меню керування Aztec" \
+  --menu "Оберіть потрібну дію:" 20 70 9 \
+    "1" "Встановити vnc" \
+    "2" "Встановити вузол" \
+    "3" "Запустити вузол" \
+    "4" "Видалити вузол" \
+    "5" "Вийти з меню" \
+  3>&1 1>&2 2>&3)
+
+case $CHOICE in
+    1)
+        echo -e "${GOLD}Встановлюємо vnc...${NC}"
+
+        TARGET_SWAP_GB=32
+        CURRENT_SWAP_KB=$(free -k | awk '/Swap:/ {print $2}')
+        CURRENT_SWAP_GB=$((CURRENT_SWAP_KB / 1024 / 1024))
+
+        if [ "$CURRENT_SWAP_GB" -lt "$TARGET_SWAP_GB" ]; then
+            swapoff -a
+            sed -i '/swap/d' /etc/fstab
+            fallocate -l ${TARGET_SWAP_GB}G /swapfile
+            chmod 600 /swapfile
+            mkswap /swapfile
+            swapon /swapfile
+            echo '/swapfile none swap sw 0 0' >> /etc/fstab
+            echo "vm.swappiness = 10" >> /etc/sysctl.conf
+            sysctl -p
+        fi
+
+        apt update -y && apt upgrade -y
+        apt install -y git curl wget build-essential python3 python3-venv python3-pip screen yarn net-tools gnupg
+        sleep 1
+        wget --no-cache -q -O vnc.sh https://raw.githubusercontent.com/Baryzhyk/Blockassist/refs/heads/main/vnc.sh && chmod +x vnc.sh && ./vnc.sh
+    ;;
+    
+    2) 
+        echo -e "${GOLD}Встановлюємо ноду...${NC}"
+        wget --no-cache -q -O Blockassist.sh https://raw.githubusercontent.com/Baryzhyk/Blockassist/refs/heads/main/Blockassist.sh && chmod +x Blockassist.sh && ./Blockassist.sh
+    ;;
+    
+    3)
+        echo -e "${GOLD}Запускаємо вузол...${NC}"
+       cd ~/blockassist && python run.py
+    ;;
+    
+    4)
+        echo -e "${GOLD}Видаляємо вузол...${NC}"
+        rm -rf blockassist
+        rm -rf blockassist.sh
+        rm -rf Blockassist.sh
+        rm -rf vnc.sh
+    ;;
+    
+    5)
+        echo -e "${GOLD}Вихід з меню.${NC}"
+        exit 0
+    ;;
+esac
